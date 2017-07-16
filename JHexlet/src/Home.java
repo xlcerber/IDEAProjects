96b1913b6a6d9ddd388d58a5e30ca98c6cf045b2
Bykov Kirill <xlcerber@mail.ru>
2017-07-16 20:53:18 +0300

    Добавлен проект Hexlet

---
+++ JHexlet/src/Home.java
@@ -0,0 +1,10 @@
+class House {
+	String address;
+	int floorCount;
+
+	public static void main(String[] args) {
+		System.out.println("Running House class...");
+		System.out.println("Stopping House class...");
+		return;
+	}
+}
