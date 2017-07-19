f1c0c005d0e71458077cd5e164579c3cd2ec229e
xlcerber <xlcerber@gmail.com>
2017-07-19 14:15:42 +0300

    Update Main.java

--- JHexlet/src/io/hexlet/java/Main.java
+++ JHexlet/src/io/hexlet/java/Main.java
@@ -1,18 +1,20 @@
 package io.hexlet.java;
 
-
 import io.hexlet.java.controller.Game;
+import io.hexlet.java.view.AdvancedConcoleView;
 import io.hexlet.java.view.ConsoleView;
 
 public class Main {
 
     public static void main(String... args) {
     	final Game game = new Game();
-    	final ConsoleView consoleview = new ConsoleView(game);
-    	startGame(consoleview);
+    	final ConsoleView consoleView = new ConsoleView(game);
+        final ConsoleView advancedConcoleView = new AdvancedConcoleView(game);
+    	startGame(consoleView);
+        startGame(advancedConcoleView);
     }
 
-    private static void startGame(final ConsoleView consoleview) {
+    private static void startGame(final ConsoleView consoleView) {
     	consoleView.showGameName();
     }
 
