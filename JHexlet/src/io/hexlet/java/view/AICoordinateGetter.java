cf04c8bb7d624eabc5f4257b9fc3df32af81b200
Bykov Kirill <xlcerber@mail.ru>
2017-07-30 22:24:20 +0300

    Добавлен AI

---
+++ JHexlet/src/io/hexlet/java/view/AICoordinateGetter.java
@@ -0,0 +1,55 @@
+package io.hexlet.java.view;
+
+import io.hexlet.java.model.Field;
+import io.hexlet.java.model.Figure;
+import io.hexlet.java.model.Point;
+import java.util.Random;
+
+public class AICoordinateGetter implements ICoordinateGetter {
+
+    public Point getMoveCoordinate(final Field field) {
+        // BEGIN (write your solution here)
+
+
+        if (field.getFigure(new Point(1, 1)) == null) {
+            return new Point(1, 1);
+        } else if (field.getFigure(new Point(1, 1)) == Figure.X) {
+
+            if (field.getFigure(new Point(0, 1)) == null) {
+                return new Point(0, 1);
+            }
+
+            if (field.getFigure(new Point(1, 0)) == null) {
+                return new Point(1, 0);
+            }
+
+            if (field.getFigure(new Point(1, 2)) == null) {
+                return new Point(1, 2);
+            }
+
+            if (field.getFigure(new Point(2, 1)) == null) {
+                return new Point(2, 1);
+            }
+        } else {
+
+            if (field.getFigure(new Point(0, 0)) == null) {
+                return new Point(0, 0);
+            }
+
+            if (field.getFigure(new Point(0, 2)) == null) {
+                return new Point(0, 2);
+            }
+
+            if (field.getFigure(new Point(2, 0)) == null) {
+                return new Point(2, 0);
+            }
+
+            if (field.getFigure(new Point(2, 2)) == null) {
+                return new Point(2, 2);
+            }
+
+        }
+
+        return null;
+    }
+}
