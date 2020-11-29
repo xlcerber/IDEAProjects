191e0183a40f262e8d987cff37380af2d4577e31
Bykov Kirill <xlcerber@gmail.com>
2020-11-29 18:42:04 +0300

    Реализовано детальный просмотр записей в ленте блога.

--- JSpringBoot/blog/src/main/java/ru/xlcerber/blog/controllers/BlogController.java
+++ JSpringBoot/blog/src/main/java/ru/xlcerber/blog/controllers/BlogController.java
@@ -4,10 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.xlcerber.blog.models.Post;
 import ru.xlcerber.blog.repo.PostRepository;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 @Controller
 public class BlogController {
@@ -25,6 +29,18 @@ public class BlogController {
     @GetMapping("/blog/add")
     public String blogAdd(Model model) {
         return "blog-add";
+    }
+
+    @GetMapping("/blog/{id}")
+    public String blogDetails(@PathVariable(value = "id") long id, Model model) {
+        if (!postRepository.existsById(id)) {
+            return "redirect:/blog";
+        }
+        Optional<Post> post = postRepository.findById(id);
+        ArrayList<Post> res = new ArrayList<>();
+        post.ifPresent(res::add);
+        model.addAttribute("post", res);
+        return "blog-details";
     }
 
     @PostMapping("/blog/add")
